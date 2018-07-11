aba20878f23aba0a9dee23a22bfab436f56cf01f
Matiboux <matiboux@gmail.com>
2018-07-11 02:43:33 +0200

    Fixed default SQL hostname

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $mailexp = ''; // Adresse de l'expediteur pour les mails
 $motdepassem = ''; // Mot de passe de l'adresse expéditeur mail
 
 /** Configuration pour la base de donnée SQL */
-define('DB_HOST', 'localhsot'); // Hôte
+define('DB_HOST', 'localhost'); // Hôte
 define('DB_NAME', 'database'); // Nom de la base de donnée
 define('DB_USER', 'root'); // Nom d'utilisateur
 define('DB_PASS', ''); // Mot de passe
