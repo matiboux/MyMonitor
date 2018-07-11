51e5561882aa46b1f742fb90ed8cc78431d52e03
Matiboux <matiboux@gmail.com>
2018-07-11 02:41:56 +0200

    Updated MySQL Config
    → Added variable for hostname

--- config.php
+++ config.php
@@ -2,7 +2,10 @@
 $hote = ''; // Adresse du serveur mail
 $mailexp = ''; // Adresse de l'expediteur pour les mails
 $motdepassem = ''; // Mot de passe de l'adresse expéditeur mail
-$user = ''; // Utilisateur base de donnée sql
-$pass = ''; // Mot de passe base de donnée sql
-$nomdb = '';
+
+/** Configuration pour la base de donnée SQL */
+define('DB_HOST', 'localhsot'); // Hôte
+define('DB_NAME', 'database'); // Nom de la base de donnée
+define('DB_USER', 'root'); // Nom d'utilisateur
+define('DB_PASS', ''); // Mot de passe
 ?>
--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,13 @@
 <?php
 include 'config.php';
+
 try
 {
-
-$bdd = new PDO('mysql:host=plesk1.dyjix.eu;dbname='.$nomdb.'', $user, $pass);
+	$bdd = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 }
 catch (Exception $e)
 {
-// En cas d'erreur, on affiche un message et on arrête tout
-die('Erreur : ' . $e->getMessage());
+	// En cas d'erreur, on affiche un message et on arrête tout
+	die('Erreur : ' . $e->getMessage());
 }
 ?>
